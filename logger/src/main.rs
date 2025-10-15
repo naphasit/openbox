83e022c1b95cd500e3060a1a31e876d03736dca5
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-15 21:00:43 +0700

    feat: write to file

---
+++ logger/src/main.rs
@@ -0,0 +1,74 @@
+use chrono::Utc;
+use colored::{Color, Colorize};
+use log::{Level, LevelFilter, Log, Metadata, Record, info};
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
+use std::sync::Mutex;
+
+// TODO: change to real path
+const LOGS_PATH: &str = "/home/naphasitng/.local/share/openbox/logs";
+
+pub struct Logger {
+    file: Mutex<File>,
+}
+
+impl Logger {
+    pub fn init() {
+        let date = Utc::now().format("%Y-%m-%d_%H:%M:%S");
+        let log_file_name = format!("{date}.log");
+        let log_path = Path::new(LOGS_PATH).join(log_file_name);
+
+        println!("{:?}", log_path);
+
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(log_path)
+            .expect("Failed to open log file");
+
+        let logger: Logger = Logger {
+            file: Mutex::new(file),
+        };
+
+        log::set_boxed_logger(Box::new(logger))
+            .map(|()| log::set_max_level(LevelFilter::Info))
+            .expect("Failed to set logger");
+    }
+}
+
+impl Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::max()
+    }
+
+    fn log(&self, record: &Record) {
+        let time = Utc::now()
+            .format("%Y-%m-%d %H:%M:%S")
+            .to_string()
+            .bright_black();
+        let level_color = match record.level() {
+            Level::Error => Color::Red,
+            Level::Warn => Color::Yellow,
+            Level::Info => Color::Blue,
+            Level::Debug => Color::Magenta,
+            Level::Trace => Color::BrightCyan,
+        };
+        let level = record.level().as_str().color(level_color);
+        let args = record.args();
+
+        let mut file = self.file.lock().unwrap();
+
+        if self.enabled(record.metadata()) {
+            println!("[{}] [{:<5}]: {}", time, level, args);
+            writeln!(file, "[{}] [{:<5}]: {}", time, level, args).unwrap();
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+fn main() {
+    Logger::init();
+    info!("HI");
+}
