dcdbecace74f4e122d1526a9dc60b9e899bb2370
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-15 21:34:25 +0700

    refactor: add comments and change name

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -18,6 +18,7 @@ pub struct Logger {
 
 impl Logger {
     pub fn init() {
+        //* ===== Log Level =====
         let log_level_env = env::var("LOG_LEVEL").unwrap_or("INFO".to_string());
         let log_level = match log_level_env.as_str() {
             "OFF" => LevelFilter::Off,
@@ -28,7 +29,11 @@ impl Logger {
             "TRACE" => LevelFilter::Trace,
             _ => LevelFilter::Info,
         };
+
+        //* ===== Timestamp =====
         let date = Utc::now().format("%Y-%m-%d_%H:%M:%S");
+
+        //* ===== Log Path =====
         let log_file_name = format!("{date}.log");
         let log_path = Path::new(LOGS_PATH).join(log_file_name);
 
@@ -42,6 +47,7 @@ impl Logger {
             file: Mutex::new(file),
         };
 
+        //* ===== Setup Logger =====
         log::set_boxed_logger(Box::new(logger))
             .map(|()| log::set_max_level(log_level))
             .expect("Failed to set logger");
@@ -54,24 +60,30 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &Record) {
-        let time = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        let time_colored = time.bright_black();
-        let level_color = match record.level() {
+        //* ===== Timestamp =====
+        let date = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        let date_colored = date.bright_black();
+
+        //* ===== Log Level =====
+        let level = record.level();
+        let level_color = match level {
             Level::Error => Color::Red,
             Level::Warn => Color::Yellow,
             Level::Info => Color::Blue,
             Level::Debug => Color::Magenta,
             Level::Trace => Color::BrightCyan,
         };
-        let level = record.level();
         let level_colored = record.level().as_str().color(level_color);
-        let args = record.args();
 
+        //* ===== Log Contents =====
+        let contents = record.args();
+
+        //* ===== Write/Print =====
         let mut file = self.file.lock().unwrap();
 
         if self.enabled(record.metadata()) {
-            println!("[{}] [{:<5}]: {}", time_colored, level_colored, args);
-            writeln!(file, "[{}] [{:<5}]: {}", time, level, args).unwrap();
+            println!("[{}] [{:<5}]: {}", date_colored, level_colored, contents);
+            writeln!(file, "[{}] [{:<5}]: {}", date, level, contents).unwrap();
         }
     }
 
