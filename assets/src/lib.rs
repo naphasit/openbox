e2af4697b9e5d3802953c17b2fffda9d566164be
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-15 22:02:52 +0700

    feat: example texture

--- assets/src/lib.rs
+++ assets/src/lib.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+pub enum Texture {
+    Dirt,
+}
+
+impl Into<Rect> for Texture {
+    fn into(self) -> Rect {
+        match self {
+            Texture::Dirt => Rect::new(0.0, 0.0, 32.0, 32.0),
+        }
+    }
 }
