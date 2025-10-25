ce560a8d81b0a2b09f8b9b2f78c4b920420b1c0f
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-25 15:36:04 +0700

    refactor: separate World and Components into module

---
+++ ecs/src/components.rs
@@ -0,0 +1,26 @@
+use crate::Entity;
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
+#[derive(Debug)]
+pub struct Components(pub HashMap<TypeId, HashMap<Entity, Box<dyn Any>>>);
+
+impl Components {
+    fn get<T: 'static>(&self) -> Option<&HashMap<Entity, Box<dyn Any>>> {
+        self.0.get(&TypeId::of::<T>())
+    }
+
+    fn get_mut<T: 'static>(&mut self) -> Option<&mut HashMap<Entity, Box<dyn Any>>> {
+        self.0.get_mut(&TypeId::of::<T>())
+    }
+
+    pub fn get_by_entity<T: 'static>(&self, entity: Entity) -> Option<&T> {
+        self.get::<T>()?.get(&entity)?.downcast_ref::<T>()
+    }
+
+    pub fn get_mut_by_entity<T: 'static>(&mut self, entity: Entity) -> Option<&mut T> {
+        self.get_mut::<T>()?.get_mut(&entity)?.downcast_mut::<T>()
+    }
+}
--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,59 +1,7 @@
-use std::{
-    any::{Any, TypeId},
-    collections::HashMap,
-};
+mod components;
+pub use components::Components;
+mod world;
+pub use world::World;
+
 use uuid::Uuid;
-
 pub type Entity = Uuid;
-
-#[derive(Debug)]
-pub struct Components(HashMap<TypeId, HashMap<Entity, Box<dyn Any>>>);
-
-pub struct World {
-    pub components: Components,
-    pub entities: Vec<Entity>,
-}
-
-impl World {
-    pub fn new() -> Self {
-        Self {
-            components: Components(HashMap::new()),
-            entities: Vec::new(),
-        }
-    }
-
-    pub fn spawn(&mut self, components: Vec<Box<dyn Any>>) -> Entity {
-        //* ===== Add Entity =====
-        let entity = Uuid::new_v4();
-        self.entities.push(entity);
-
-        //* ===== Add Components =====
-        for c in components {
-            let type_id = (*c).type_id();
-            let entry = self.components.0.entry(type_id).or_insert(HashMap::new());
-
-            entry.insert(entity, c);
-        }
-
-        //* ===== Return =====
-        entity
-    }
-}
-
-impl Components {
-    fn get<T: 'static>(&self) -> Option<&HashMap<Entity, Box<dyn Any>>> {
-        self.0.get(&TypeId::of::<T>())
-    }
-
-    fn get_mut<T: 'static>(&mut self) -> Option<&mut HashMap<Entity, Box<dyn Any>>> {
-        self.0.get_mut(&TypeId::of::<T>())
-    }
-
-    pub fn get_by_entity<T: 'static>(&self, entity: Entity) -> Option<&T> {
-        self.get::<T>()?.get(&entity)?.downcast_ref::<T>()
-    }
-
-    pub fn get_mut_by_entity<T: 'static>(&mut self, entity: Entity) -> Option<&mut T> {
-        self.get_mut::<T>()?.get_mut(&entity)?.downcast_mut::<T>()
-    }
-}
