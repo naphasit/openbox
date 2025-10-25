1e7dcd93e2f26f184595cad36473f9ad76012d0b
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-25 15:39:50 +0700

    feat: impl Systems

--- ecs/src/systems.rs
+++ ecs/src/systems.rs
@@ -5,6 +5,37 @@ pub struct Systems {
     update: Vec<Box<dyn FnMut(&mut World)>>,
 }
 
+impl Systems {
+    pub fn new() -> Self {
+        Self {
+            startup: Vec::new(),
+            update: Vec::new(),
+        }
+    }
+
+    pub fn run(&mut self, world: &mut World) {
+        //* ===== Startup =====
+        for sys in self.startup.iter_mut() {
+            sys(world);
+        }
+
+        //* ===== Update =====
+        loop {
+            for sys in self.update.iter_mut() {
+                sys(world);
+            }
+        }
+    }
+
+    pub fn add(&mut self, sys_type: SystemType, sys: Box<dyn FnMut(&mut World)>) -> &Self {
+        match sys_type {
+            SystemType::Startup => self.startup.push(sys),
+            SystemType::Update => self.update.push(sys),
+        }
+        self
+    }
+}
+
 pub enum SystemType {
     Startup,
     Update,
