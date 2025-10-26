b25a3a11189d1ffe7200e8edd763aa8892788f25
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-26 15:09:51 +0700

    feat(entity): spawn

--- state/src/world.rs
+++ state/src/world.rs
@@ -8,3 +8,22 @@ pub struct World {
     entities: Vec<Uuid>,
     components: HashMap<TypeId, HashMap<Uuid, Box<dyn Any>>>,
 }
+
+impl World {
+    pub fn spawn(&mut self, components: Vec<Box<dyn Any>>) -> Uuid {
+        //* ===== Add Entity =====
+        let entity = Uuid::new_v4();
+        self.entities.push(entity);
+
+        //* ===== Add Components =====
+        for c in components {
+            let type_id = (*c).type_id();
+            let entry = self.components.entry(type_id).or_insert(HashMap::new());
+
+            entry.insert(entity, c);
+        }
+
+        //* ===== Return =====
+        entity
+    }
+}
