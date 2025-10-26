e7815aacd45ec5e65586ffffca6aa3fc189c57e8
naphasit <204818954+naphasit@users.noreply.github.com>
2025-10-26 16:46:39 +0700

    feat(schedule): new

--- state/src/schedule.rs
+++ state/src/schedule.rs
@@ -4,3 +4,12 @@ pub struct Schedule {
     startup: Vec<Box<dyn FnMut(&mut World)>>,
     update: Vec<Box<dyn FnMut(&mut World)>>,
 }
+
+impl Schedule {
+    pub fn new() -> Self {
+        Self {
+            startup: Vec::new(),
+            update: Vec::new(),
+        }
+    }
+}
